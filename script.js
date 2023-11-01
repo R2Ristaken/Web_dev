8e8ba642016c847f22ea75708dc181396d7763d7
Ramesh <rameshsrkme@gmail.com>
2023-11-01 14:33:17 +0530

    updates

--- script.js
+++ script.js
@@ -28,11 +28,57 @@ function firstPageAnim(){
     })
 }
 
-function circleMouseFollower(){
+var timer ;
+
+function circlesquse(){
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
     window.addEventListener("mousemove", function(dets){
-        document.querySelector("#circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+        this.clearTimeout(timer);
+
+        xscale = gsap.utils.clamp(.8,1,2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8,1,2, dets.clientY - yprev);
+    
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+        
+        circleMouseFollower(xscale, yscale);
+
+        timer = setTimeout(function(){
+            document.querySelector("#circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+        }, 100);
+    });
+}
+
+
+function circleMouseFollower(xscale, yscale){
+    window.addEventListener("mousemove", function(dets){
+        document.querySelector("#circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
     })
 }
 
+circlesquse();
 circleMouseFollower();
 firstPageAnim();
+
+
+document.querySelectorAll(".elem").forEach(function(elem) {
+    var rotate = 0;
+    var diffrot = 0;
+
+    elem.addEventListener("mousemove", function(dets) {
+        var diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            
+            top: diff,
+            left: dets.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot * .2),
+        });
+    });
+});
