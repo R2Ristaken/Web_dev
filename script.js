8966c45f64909f5c9e28c25c8d48eff416af3f2f
Ramesh <rameshsrkme@gmail.com>
2023-11-02 11:13:14 +0530

    bug fix

--- script.js
+++ script.js
@@ -65,20 +65,28 @@ circleMouseFollower();
 firstPageAnim();
 
 
-document.querySelectorAll(".elem").forEach(function(elem) {
+document.querySelectorAll(".elem").forEach(function (elem) {
     var rotate = 0;
     var diffrot = 0;
-
-    elem.addEventListener("mousemove", function(dets) {
-        var diff = dets.clientY - elem.getBoundingClientRect().top;
-        diffrot = dets.clientX - rotate;
-        rotate = dets.clientX;
-        gsap.to(elem.querySelector("img"), {
-            opacity: 1,
-            
-            top: diff,
-            left: dets.clientX,
-            rotate: gsap.utils.clamp(-20, 20, diffrot * .2),
-        });
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
     });
-});
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
+  });
